0fc7e14a05d1563bd183b06ac5160d14a63efe7b
alex.kim <alkkim@gmail.com>
2023-04-08 22:36:14 -0700

    added thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,22 @@
 const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController');
 
+// GET and POST api route for /thoughts
+router.route('/').get(getThoughts).post(createThought);
+// GET, PUT and DELETE api route for thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+// POST api route for /thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+// DELETE api route /thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 
 module.exports = router;
