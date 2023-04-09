f961abbc0d76a95643c6bf65712d60db880e1118
alex.kim <alkkim@gmail.com>
2023-04-08 20:35:43 -0700

    added create, update and delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,7 +30,57 @@ const userController = {
         res.status(500).json(err);
       });
   },
-}
-
-
+  // Create user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // Update single user info
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      {
+        $set: req.body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'no user found' });
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // delete user data
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'no user found' });
+        }
+        return Thought.deleteMany({ _id: { $in: userData.thoughts } });
+      })
+      .then(() => {
+        res.json({ message: 'user data deleted' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
 module.exports = userController;
+
