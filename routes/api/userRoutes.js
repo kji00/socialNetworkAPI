3bfef7fb21780adf3f348808008d7d3a34186a58
alex.kim <alkkim@gmail.com>
2023-04-08 21:59:45 -0700

    added all user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+// GET and POST api routes for /users
+router.route('/').get(getUsers).post(createUser);
+// GET, PUT and DELETE api route for /users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+// POST and DELETE api routes for /users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 
 module.exports = router;
