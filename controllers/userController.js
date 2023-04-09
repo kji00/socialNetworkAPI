00e235b3fd1cd228fe3e1dec21f9036ac5968d7c
alex.kim <alkkim@gmail.com>
2023-04-08 19:43:54 -0700

    created userController and get all users and single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,35 @@
 const { User, Thought } = require('../models');
 
 const userController = {
-  
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .select('-__v')
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // Get a user by id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('friends')
+      .populate('thoughts')
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'no user found' });
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 }
 
 
